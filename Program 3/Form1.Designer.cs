aeb6a2ed391db3c1983b724fd6526476737cc343
kelec <kelec@LAPTOP-JGFOV90A>
2023-04-02 17:12:38 -0400

    Add project files.

---
+++ Program 3/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Program_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gardenLbl = new System.Windows.Forms.Label();
+            this.entreeLbl = new System.Windows.Forms.Label();
+            this.Qty = new System.Windows.Forms.Label();
+            this.typeComboBox = new System.Windows.Forms.ComboBox();
+            this.entreeBox = new System.Windows.Forms.TextBox();
+            this.quantityBox = new System.Windows.Forms.TextBox();
+            this.flowerbutton = new System.Windows.Forms.Button();
+            this.costLbl = new System.Windows.Forms.Label();
+            this.baseLbl = new System.Windows.Forms.Label();
+            this.discountLbl = new System.Windows.Forms.Label();
+            this.totalLbl = new System.Windows.Forms.Label();
+            this.outputFlowers = new System.Windows.Forms.Label();
+            this.outputBase = new System.Windows.Forms.Label();
+            this.outputDiscount = new System.Windows.Forms.Label();
+            this.outputTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gardenLbl
+            // 
+            this.gardenLbl.AutoSize = true;
+            this.gardenLbl.Location = new System.Drawing.Point(137, 56);
+            this.gardenLbl.Name = "gardenLbl";
+            this.gardenLbl.Size = new System.Drawing.Size(45, 13);
+            this.gardenLbl.TabIndex = 0;
+            this.gardenLbl.Text = "Garden:";
+            // 
+            // entreeLbl
+            // 
+            this.entreeLbl.AutoSize = true;
+            this.entreeLbl.Location = new System.Drawing.Point(76, 84);
+            this.entreeLbl.Name = "entreeLbl";
+            this.entreeLbl.Size = new System.Drawing.Size(106, 13);
+            this.entreeLbl.TabIndex = 1;
+            this.entreeLbl.Text = "Entree/Item Number:";
+            // 
+            // Qty
+            // 
+            this.Qty.AutoSize = true;
+            this.Qty.Location = new System.Drawing.Point(133, 113);
+            this.Qty.Name = "Qty";
+            this.Qty.Size = new System.Drawing.Size(49, 13);
+            this.Qty.TabIndex = 2;
+            this.Qty.Text = "Quantity:";
+            // 
+            // typeComboBox
+            // 
+            this.typeComboBox.AutoCompleteCustomSource.AddRange(new string[] {
+            "Premium",
+            "Standard",
+            "Discount"});
+            this.typeComboBox.FormattingEnabled = true;
+            this.typeComboBox.Items.AddRange(new object[] {
+            "",
+            "Premium",
+            "Standard",
+            "Discount"});
+            this.typeComboBox.Location = new System.Drawing.Point(188, 53);
+            this.typeComboBox.Name = "typeComboBox";
+            this.typeComboBox.Size = new System.Drawing.Size(100, 21);
+            this.typeComboBox.TabIndex = 3;
+            // 
+            // entreeBox
+            // 
+            this.entreeBox.Location = new System.Drawing.Point(188, 80);
+            this.entreeBox.Name = "entreeBox";
+            this.entreeBox.Size = new System.Drawing.Size(100, 20);
+            this.entreeBox.TabIndex = 4;
+            // 
+            // quantityBox
+            // 
+            this.quantityBox.Location = new System.Drawing.Point(188, 110);
+            this.quantityBox.Name = "quantityBox";
+            this.quantityBox.Size = new System.Drawing.Size(100, 20);
+            this.quantityBox.TabIndex = 5;
+            // 
+            // flowerbutton
+            // 
+            this.flowerbutton.Location = new System.Drawing.Point(200, 152);
+            this.flowerbutton.Name = "flowerbutton";
+            this.flowerbutton.Size = new System.Drawing.Size(75, 23);
+            this.flowerbutton.TabIndex = 6;
+            this.flowerbutton.Text = "Calculate";
+            this.flowerbutton.UseVisualStyleBackColor = true;
+            this.flowerbutton.Click += new System.EventHandler(this.flowerbutton_Click);
+            // 
+            // costLbl
+            // 
+            this.costLbl.AutoSize = true;
+            this.costLbl.Location = new System.Drawing.Point(112, 193);
+            this.costLbl.Name = "costLbl";
+            this.costLbl.Size = new System.Drawing.Size(70, 13);
+            this.costLbl.TabIndex = 7;
+            this.costLbl.Text = "Flowers Cost:";
+            // 
+            // baseLbl
+            // 
+            this.baseLbl.AutoSize = true;
+            this.baseLbl.Location = new System.Drawing.Point(80, 218);
+            this.baseLbl.Name = "baseLbl";
+            this.baseLbl.Size = new System.Drawing.Size(102, 13);
+            this.baseLbl.TabIndex = 8;
+            this.baseLbl.Text = "Base Adjusted Cost:";
+            // 
+            // discountLbl
+            // 
+            this.discountLbl.AutoSize = true;
+            this.discountLbl.Location = new System.Drawing.Point(87, 250);
+            this.discountLbl.Name = "discountLbl";
+            this.discountLbl.Size = new System.Drawing.Size(95, 13);
+            this.discountLbl.TabIndex = 9;
+            this.discountLbl.Text = "Discount Percent: ";
+            // 
+            // totalLbl
+            // 
+            this.totalLbl.AutoSize = true;
+            this.totalLbl.Location = new System.Drawing.Point(121, 273);
+            this.totalLbl.Name = "totalLbl";
+            this.totalLbl.Size = new System.Drawing.Size(61, 13);
+            this.totalLbl.TabIndex = 10;
+            this.totalLbl.Text = "Total Price:";
+            // 
+            // outputFlowers
+            // 
+            this.outputFlowers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputFlowers.Location = new System.Drawing.Point(188, 185);
+            this.outputFlowers.Name = "outputFlowers";
+            this.outputFlowers.Size = new System.Drawing.Size(100, 21);
+            this.outputFlowers.TabIndex = 11;
+            // 
+            // outputBase
+            // 
+            this.outputBase.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputBase.Location = new System.Drawing.Point(188, 210);
+            this.outputBase.Name = "outputBase";
+            this.outputBase.Size = new System.Drawing.Size(100, 21);
+            this.outputBase.TabIndex = 12;
+            // 
+            // outputDiscount
+            // 
+            this.outputDiscount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputDiscount.Location = new System.Drawing.Point(188, 240);
+            this.outputDiscount.Name = "outputDiscount";
+            this.outputDiscount.Size = new System.Drawing.Size(100, 23);
+            this.outputDiscount.TabIndex = 13;
+            // 
+            // outputTotal
+            // 
+            this.outputTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputTotal.Location = new System.Drawing.Point(188, 272);
+            this.outputTotal.Name = "outputTotal";
+            this.outputTotal.Size = new System.Drawing.Size(100, 23);
+            this.outputTotal.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.flowerbutton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 347);
+            this.Controls.Add(this.outputTotal);
+            this.Controls.Add(this.outputDiscount);
+            this.Controls.Add(this.outputBase);
+            this.Controls.Add(this.outputFlowers);
+            this.Controls.Add(this.totalLbl);
+            this.Controls.Add(this.discountLbl);
+            this.Controls.Add(this.baseLbl);
+            this.Controls.Add(this.costLbl);
+            this.Controls.Add(this.flowerbutton);
+            this.Controls.Add(this.quantityBox);
+            this.Controls.Add(this.entreeBox);
+            this.Controls.Add(this.typeComboBox);
+            this.Controls.Add(this.Qty);
+            this.Controls.Add(this.entreeLbl);
+            this.Controls.Add(this.gardenLbl);
+            this.Name = "Form1";
+            this.Text = "The Happy Garden";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label gardenLbl;
+        private System.Windows.Forms.Label entreeLbl;
+        private System.Windows.Forms.Label Qty;
+        private System.Windows.Forms.ComboBox typeComboBox;
+        private System.Windows.Forms.TextBox entreeBox;
+        private System.Windows.Forms.TextBox quantityBox;
+        private System.Windows.Forms.Button flowerbutton;
+        private System.Windows.Forms.Label costLbl;
+        private System.Windows.Forms.Label baseLbl;
+        private System.Windows.Forms.Label discountLbl;
+        private System.Windows.Forms.Label totalLbl;
+        private System.Windows.Forms.Label outputFlowers;
+        private System.Windows.Forms.Label outputBase;
+        private System.Windows.Forms.Label outputDiscount;
+        private System.Windows.Forms.Label outputTotal;
+    }
+}
+
---
+++ Program 3/Form1.cs
@@ -0,0 +1,140 @@
+﻿//Grading ID: R2226
+//Program 3
+//04/01/2022
+//CIS 199-02
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //These are the arrays that are being used and established for the Program.
+        string[] type = { "Premium", "Standard", "Discount" };
+        double[] baseRate = { 1.1, 1, 0.9 };
+        int[] number = { 10001, 10002, 10003, 10004, 10005, 10006, 10007 };
+        double[] cost = { 7.87, 9.51, 10.73, 9.99, 11.99, 5.00, 4.58 };
+        int[] discountRange = { 0, 6, 16, 21 };
+        double[] discounts = { 1, 0.95, 0.90, 0.85 };
+        
+        private void flowerbutton_Click(object sender, EventArgs e)
+        {
+            //These are the variables that will be implemented for the project.
+            int nums = 0;
+            double flowerCost = 0;
+            double based = 0;
+            double perFlower = 0;
+            double customer = 0;
+            double baseA = 0;
+            double total;
+            int quantity = 0;
+            bool flowerFound = false;
+            bool numberCost = false;
+            bool qtyFound = false;
+
+            //This is the code for the combo box for the GUI application and it is done to count the arrays in the code
+            for( int x = 0; x < type.Length && !flowerFound; ++x)
+            {
+                if(typeComboBox.SelectedIndex >= 0)
+                {
+                    if(typeComboBox.SelectedIndex == 1)
+                    {
+                        flowerFound = true;
+                        based = 1.1;
+                    }
+                    else if(typeComboBox.SelectedIndex == 2)
+                    {
+                        flowerFound = true;
+                        based = 1;
+                    }
+                    else
+                    {
+                        flowerFound= true;
+                        based = 0.90;
+                    }
+                }
+            }
+            //This if-else statement is here just in case someone were to not make the bool variable true.
+            if(flowerFound)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Error you may have to retype");
+            }
+
+            //This is a TryParse and a nested loop that involves the use of arrays for an output label.
+            if (int.TryParse(entreeBox.Text, out nums) && nums > 10000 && nums < 10008)
+            {
+                for (int x = 0; x < number.Length && !numberCost; ++x)
+                {
+                    if(nums == number[x])
+                    {
+                        numberCost = true;
+                        perFlower = cost[x];
+                    }
+                }
+            }
+            //This an if-else statement that is here just in case someone were not able to put in the correct information for the GUI.
+            if(numberCost)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Error so retry");
+            }
+            //This int is here to help the if-else and nested loop statement down below.
+            int index = discountRange.Length - 1; 
+            //A TryParse that involves tha use of the a while loop for another textbox in the GUI but for discounts in the array.
+            if(int.TryParse(quantityBox.Text, out quantity) && quantity >= 0)
+            {
+               while(index >= 0 && !qtyFound)
+                {
+                    if(quantity >= discountRange[index])
+                    {
+                        qtyFound = true;
+                    }
+                    else
+                    {
+                        --index;
+                    }
+                }
+            }
+            //This if-else statement is here for all the equations and output labels for the GUI and including an error message in case. 
+            if(qtyFound)
+            {
+                customer = 1 - discounts[index];
+                flowerCost = quantity * perFlower;
+                baseA = flowerCost * based;
+                total = baseA - (baseA * customer);
+                outputFlowers.Text = $"{flowerCost:C2}";
+                outputDiscount.Text = $"{customer:P}";
+                outputBase.Text = $"{baseA:C2}";
+                outputTotal.Text = $"{total:C2}";
+            }
+            else
+            {
+                MessageBox.Show("Error so retry");
+            }
+
+
+
+
+
+
+        }
+    }
+}
